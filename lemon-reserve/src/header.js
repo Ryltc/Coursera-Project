a914f006d798e4418db0438122f2c2b865953ab6
otmp <47669979+Ryltc@users.noreply.github.com>
2023-04-13 14:53:44 -0400

    created header
    added img an nav assets

---
+++ lemon-reserve/src/header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Nav} from './Nav';
+import { ReactComponent as Logo } from './icon_assets./Logo.svg';
+
+const Header = () => {
+    return (
+        <>
+        <div>
+            <img src={Logo} alt="Logo"/>
+        </div>
+        <Nav>
+            <nav>
+                <ul>
+                    <li><a href="/home/">Homw</a></li>
+                    <li><a href="/about/">About</a></li>
+                    <li><a href="/menu/">menu</a></li>
+                    <li><a href="/reservations/">Reservations</a></li>
+                    <li><a href="/order-online/">Order Online</a></li>
+                    <li><a href="/login/">Login</a></li>
+                </ul>
+            </nav>
+        </Nav>
+        </>
+    )
+}
