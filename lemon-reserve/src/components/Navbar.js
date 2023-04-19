4fe7a9057249f4f2f95f168366c8f6bef9122928
otmp <47669979+Ryltc@users.noreply.github.com>
2023-04-19 12:18:16 -0400

    logo and nav

--- lemon-reserve/src/components/Navbar.js
+++ lemon-reserve/src/components/Navbar.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import { ReactComponent as Logo } from './Logo.svg';
 
 function Navbar() {
     return (
         <>
-            <nav className='navbar'>
-                <div className='navbar-container'>
-                    <ul>
-                        <li><a href="/home">Home</a></li>
-                        <li><a href="/about">About</a></li>
-                        <li><a href="/menu">menu</a></li>
-                        <li><a href="/reservations">Reservations</a></li>
-                        <li><a href="/order-online">Order Online</a></li>
-                        <li><a href="/login">Login</a></li>
-                    </ul>
+            <nav className='navigation-bar'>
+                <div className='navigation-container'>
+                    <Logo/>
+                        <ul>
+                            <li>
+                                <a href="/home">Home</a>
+                            </li>
+                            <li>
+                                <a href="/about">About</a>
+                            </li>
+                            <li>
+                                <a href="/menu">Menu</a>
+                            </li>
+                            <li>
+                                <a href="/reservations">Reservations</a>
+                            </li>
+                            <li>
+                                <a href="/order-online">Order Online</a>
+                            </li>
+                            <li>
+                                <a href="/login">Login</a>
+                            </li>
+                        </ul>
                 </div>
             </nav>
         </>
--- lemon-reserve/src/header.js
+++ lemon-reserve/src/header.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { ReactComponent as Logo } from './Logo .svg';
+import { ReactComponent as Logo } from './Logo.svg';
 import  Navbar from './components/Navbar';
 
 function header() {
     return (
         <>
-            <div id="header-img">
-                <img src={Logo} alt="Logo"/>
-            </div>
             <Navbar />
         </>
     )
