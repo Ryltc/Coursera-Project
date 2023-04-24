9039a3288d7b776de52c672fde4c780bb7f3c92e
otmp <47669979+Ryltc@users.noreply.github.com>
2023-04-24 11:25:50 -0400

    continuation of nav center

--- lemon-reserve/src/index.js
+++ lemon-reserve/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 <head>
   <meta name="description" content="Restaurant Reservation App"/>
   <meta name="og:title" content="Little Lemon Reservations"/>
