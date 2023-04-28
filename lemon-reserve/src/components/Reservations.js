836ea6a1e075e7e2d354effbcc61c0f5d8f5be8e
otmp <47669979+Ryltc@users.noreply.github.com>
2023-04-28 14:10:13 -0400

    Update Reservations.js
    still working out the kinks for multiple dropdown menus

--- lemon-reserve/src/components/Reservations.js
+++ lemon-reserve/src/components/Reservations.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import DatePicker from 'react-datepicker';
+import TimePicker from 'react-dropdown-timepicker';
 import  '.Reservations.css';
 
-function Reservations() {
-    return(
-        <>
-        <div class="res-container">
-            <div class="res-imgs">
 
-            </div>
-            <div class="res-form">
+const DatePicker = () => {
+    const [startDate, setStartDate] = useState (new Date());
 
-            </div>
-        </div>
-        </>
-    )
-}
+    return (
+        <DatePicker
+        selected={startDate}
+        onChange={date => setStartDate(date)}
+        />
+    );
+};
+
+export default DatePicker
+
+
+
